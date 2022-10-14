930f232982b72aa0e2009ea3140245d87a20a163
Sumio K <tap.miwo@gmail.com>
2022-10-14 23:30:47 +0900

    17-27

--- src/App.js
+++ src/App.js
@@ -8,17 +8,34 @@ function App() {
   const [query, setQuery] = useState('apple');
   useEffect(() => {
     console.log('useEffect runnning')
-    fetch(`https://api.unsplash.com/serch/photos?query=${query}&client_id=${process.env}.React_APP_CLIENT_ID`)
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
     .then(response => response.json())
     .then(data => {
       console.log(data)
       setImages(data.results)
-    },[query])
-  })
+    })
+  },[query])
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setQuery(text);
+    setText('');
+    console.log("onSubmit is runing")
+  }
   return (
     <div className="App">
-
+      <div className='main'>
+        <form onSubmit={onSubmit}>
+          <input
+          type="text"
+          onChange={e => setText(e.target.value)}
+          />
+          <button type="submit">
+            serch
+          </button>
+        </form>
+      </div>
+      
     </div>
   );
 }
